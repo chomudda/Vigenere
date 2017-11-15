887d611cf8008bd42b767cc28459cf5247a3667c
chomudda <justin.phan@hotmail.com>
2017-11-15 20:58:07 +1100

    Onclick method adjustments

--- app/src/main/java/today/duma/vigenere/MainActivity.java
+++ app/src/main/java/today/duma/vigenere/MainActivity.java
@@ -2,11 +2,9 @@ package today.duma.vigenere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View.OnClickListener;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.EditText;
-import android.widget.Button;
 import android.widget.Toast;
 import android.view.View;
 import java.util.Random;
@@ -17,22 +15,19 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     //Widgets
-    private EditText message;
-    private EditText key;
-    private Button encrypt;
-    private Button decrypt;
-    private Button key_gen;
-    private RadioGroup radioGroup;
+    public EditText message;
+    public EditText key;
+    public RadioGroup radioGroup;
 
     //Global arrays
-    public static final ArrayList config = new ArrayList();
-    public static final ArrayList message_no = new ArrayList();
-    public static final ArrayList key_no = new ArrayList();
-    public static final ArrayList wrap = new ArrayList();
-    public static final ArrayList total = new ArrayList();
-    public static final ArrayList fin = new ArrayList();
-    public static final ArrayList key_array = new ArrayList();
-    public static final Random rand = new Random();
+    public ArrayList config = new ArrayList();
+    public ArrayList message_no = new ArrayList();
+    public ArrayList key_no = new ArrayList();
+    public ArrayList wrap = new ArrayList();
+    public ArrayList total = new ArrayList();
+    public ArrayList fin = new ArrayList();
+    public  ArrayList key_array = new ArrayList();
+    public Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,169 +36,133 @@ public class MainActivity extends AppCompatActivity {
 
         message = (EditText)findViewById(R.id.message);
         key = (EditText)findViewById(R.id.key);
-        encrypt = (Button)findViewById(R.id.encrypt);
-        decrypt = (Button)findViewById(R.id.decrypt);
-        key_gen = (Button)findViewById(R.id.key_gen);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-
-        //ENCRYPT BUTTON
-        encrypt.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                //Clear arrays
-                message_no.clear();
-                key_no.clear();
-                wrap.clear();
-                total.clear();
-                fin.clear();
-
-                if(radioGroup.getCheckedRadioButtonId()==-1){
-                    Toast.makeText(getApplicationContext(), "Method not selected", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(message.getText().toString().matches("")){
-                    Toast.makeText(getApplicationContext(), "Please enter a message", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(key.getText().toString().matches("")){
-                    Toast.makeText(getApplicationContext(), "Please enter a key", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                //Message to values
-                for(int i=0; i<message.getText().length(); i++){
-                    message_no.add(config.indexOf(message.getText().charAt(i)));
-                }
-                //Key to values
-                for(int i=0; i<key.getText().length(); i++){
-                    key_no.add(config.indexOf(key.getText().charAt(i)));
-                }
-                //Wrap key to message
-                int c=0;
-                for(int i=0; i<message_no.size(); i++){
-                    wrap.add(key_no.get(c));
-                    c++;
-                    if(c>key_no.size()-1){
-                        c=0;
-                    }
-                }
-                //Sum wrap values with message values
-                for(int i=0; i<message_no.size(); i++){
-                    total.add(Integer.parseInt(String.valueOf(message_no.get(i)))+Integer.parseInt(String.valueOf(wrap.get(i))));
-                }
-                //Error handle values > config length
-                for(int i=0; i<total.size(); i++){
-                    if(Integer.parseInt(String.valueOf(total.get(i)))>config.size()-1){
-                        total.set(i, Integer.parseInt(String.valueOf(total.get(i)))-(config.size()-1)-1);
-                    }
-                }
-                //Get message from value
-                for(int i=0; i<total.size(); i++){
-                    fin.add(config.get(Integer.parseInt(String.valueOf(total.get(i)))));
-                }
-                //Array to string
-                StringBuilder strBuilder = new StringBuilder();
-                for (int i = 0; i < fin.size(); i++) {
-                    strBuilder.append(fin.get(i));
-                }
-                String newString = strBuilder.toString();
-                System.out.println(newString);
-                message.setText(newString);
-            }
-        });
-        //Decrypt BUTTON
-        decrypt.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                //Clear arrays
-                message_no.clear();
-                key_no.clear();
-                wrap.clear();
-                total.clear();
-                fin.clear();
-
-                if(radioGroup.getCheckedRadioButtonId()==-1){
-                    Toast.makeText(getApplicationContext(), "Method not selected", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(message.getText().toString().matches("")){
-                    Toast.makeText(getApplicationContext(), "Please enter a message", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(key.getText().toString().matches("")){
-                    Toast.makeText(getApplicationContext(), "Please enter a key", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                //Message to values
-                for(int i=0; i<message.getText().length(); i++){
-                    message_no.add(config.indexOf(message.getText().charAt(i)));
-                }
-                //Key to values
-                for(int i=0; i<key.getText().length(); i++){
-                    key_no.add(config.indexOf(key.getText().charAt(i)));
-                }
-                //Wrap key to message
-                int c=0;
-                for(int i=0; i<message_no.size(); i++){
-                    wrap.add(key_no.get(c));
-                    c++;
-                    if(c>key_no.size()-1){
-                        c=0;
-                    }
-                }
-                //Sum wrap values with message values
-                for(int i=0; i<message_no.size(); i++){
-                    total.add(Integer.parseInt(String.valueOf(message_no.get(i)))-Integer.parseInt(String.valueOf(wrap.get(i))));
-                }
-                //If total < 0, make positive
-                for(int i=0; i<total.size(); i++){
-                    if(Integer.parseInt(String.valueOf(total.get(i)))<0){
-                        total.set(i, Integer.parseInt(String.valueOf(total.get(i)))+(config.size()));
-                    }
-                }
-                //Get message from value
-                for(int i=0; i<total.size(); i++){
-                    fin.add(config.get(Integer.parseInt(String.valueOf(total.get(i)))));
-                }
-                //Array to string
-                StringBuilder strBuilder = new StringBuilder();
-                for (int i = 0; i < fin.size(); i++) {
-                    strBuilder.append(fin.get(i));
-                }
-                String newString = strBuilder.toString();
-                System.out.println(newString);
-                message.setText(newString);
-            }
-        });
-
-        //Key Gen BUTTON
-        key_gen.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                key_array.clear();
-
-                if(radioGroup.getCheckedRadioButtonId()==-1){
-                    Toast.makeText(getApplicationContext(), "Method not selected", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if(message.getText().toString().matches("")){
-                    Toast.makeText(getApplicationContext(), "Please enter a message", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                for (int i = 0; i < message.getText().toString().length(); i++) {
-                    int  j = rand.nextInt(config.size());
-                    key_array.add(config.get(j));
-                }
-
-                //Array to string
-                StringBuilder strBuilder = new StringBuilder();
-                for (int i = 0; i < key_array.size(); i++) {
-                    strBuilder.append(key_array.get(i));
-                }
-                String newString = strBuilder.toString();
-                System.out.println(newString);
-                key.setText(newString);
-            }
-        });
     }
+
+    //ENCRYPT
+    public void encrypt(View view){
+        //Clear arrays
+        message_no.clear();
+        key_no.clear();
+        wrap.clear();
+        total.clear();
+        fin.clear();
+
+        if(radioGroup.getCheckedRadioButtonId()==-1){
+            Toast.makeText(getApplicationContext(), "Method not selected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(message.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(), "Please enter a message", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(key.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(), "Please enter a key", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Message to values
+        for(int i=0; i<message.getText().length(); i++){
+            message_no.add(config.indexOf(message.getText().charAt(i)));
+        }
+        //Key to values
+        for(int i=0; i<key.getText().length(); i++){
+            key_no.add(config.indexOf(key.getText().charAt(i)));
+        }
+        //Wrap key to message
+        int c=0;
+        for(int i=0; i<message_no.size(); i++){
+            wrap.add(key_no.get(c));
+            c++;
+            if(c>key_no.size()-1){
+                c=0;
+            }
+        }
+        //Sum wrap values with message values
+        for(int i=0; i<message_no.size(); i++){
+            total.add(Integer.parseInt(String.valueOf(message_no.get(i)))+Integer.parseInt(String.valueOf(wrap.get(i))));
+        }
+        //Error handle values > config length
+        for(int i=0; i<total.size(); i++){
+            if(Integer.parseInt(String.valueOf(total.get(i)))>config.size()-1){
+                total.set(i, Integer.parseInt(String.valueOf(total.get(i)))-(config.size()-1)-1);
+            }
+        }
+        //Get message from value
+        for(int i=0; i<total.size(); i++){
+            fin.add(config.get(Integer.parseInt(String.valueOf(total.get(i)))));
+        }
+        //Array to string
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < fin.size(); i++) {
+            strBuilder.append(fin.get(i));
+        }
+        String newString = strBuilder.toString();
+        System.out.println(newString);
+        message.setText(newString);
+    }
+
+    //DECRYPT
+    public void decrypt(View view){
+        //Clear arrays
+        message_no.clear();
+        key_no.clear();
+        wrap.clear();
+        total.clear();
+        fin.clear();
+
+        if(radioGroup.getCheckedRadioButtonId()==-1){
+            Toast.makeText(getApplicationContext(), "Method not selected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(message.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(), "Please enter a message", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(key.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(), "Please enter a key", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //Message to values
+        for(int i=0; i<message.getText().length(); i++){
+            message_no.add(config.indexOf(message.getText().charAt(i)));
+        }
+        //Key to values
+        for(int i=0; i<key.getText().length(); i++){
+            key_no.add(config.indexOf(key.getText().charAt(i)));
+        }
+        //Wrap key to message
+        int c=0;
+        for(int i=0; i<message_no.size(); i++){
+            wrap.add(key_no.get(c));
+            c++;
+            if(c>key_no.size()-1){
+                c=0;
+            }
+        }
+        //Sum wrap values with message values
+        for(int i=0; i<message_no.size(); i++){
+            total.add(Integer.parseInt(String.valueOf(message_no.get(i)))-Integer.parseInt(String.valueOf(wrap.get(i))));
+        }
+        //If total < 0, make positive
+        for(int i=0; i<total.size(); i++){
+            if(Integer.parseInt(String.valueOf(total.get(i)))<0){
+                total.set(i, Integer.parseInt(String.valueOf(total.get(i)))+(config.size()));
+            }
+        }
+        //Get message from value
+        for(int i=0; i<total.size(); i++){
+            fin.add(config.get(Integer.parseInt(String.valueOf(total.get(i)))));
+        }
+        //Array to string
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < fin.size(); i++) {
+            strBuilder.append(fin.get(i));
+        }
+        String newString = strBuilder.toString();
+        System.out.println(newString);
+        message.setText(newString);
+    }
+
     //RADIO
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
@@ -233,5 +192,39 @@ public class MainActivity extends AppCompatActivity {
                 //System.out.println(config);
                 break;
         }
+    }
+
+    //KEY GEN
+    public void key_gen(View view){
+        key_array.clear();
+
+        if(radioGroup.getCheckedRadioButtonId()==-1){
+            Toast.makeText(getApplicationContext(), "Method not selected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(message.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(), "Please enter a message", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        for (int i = 0; i < message.getText().toString().length(); i++) {
+            int  j = rand.nextInt(config.size());
+            key_array.add(config.get(j));
+        }
+
+        //Array to string
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < key_array.size(); i++) {
+            strBuilder.append(key_array.get(i));
+        }
+        String newString = strBuilder.toString();
+        System.out.println(newString);
+        key.setText(newString);
+    }
+
+    //FLOATING ACTION BUTTON
+    public void fab(View view){
+
     }
 }
