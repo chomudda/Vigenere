e3edde0984cd9904703b591dc05198c6add7631b
chomudda <justin.phan@hotmail.com>
2017-11-15 21:45:56 +1100

    Added save function for keys

--- app/src/main/java/today/duma/vigenere/MainActivity.java
+++ app/src/main/java/today/duma/vigenere/MainActivity.java
@@ -1,12 +1,26 @@
 package today.duma.vigenere;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
 
 import java.util.ArrayList;
@@ -14,10 +28,34 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    /**
+     * Checks if the app has permission to write to device storage
+     * If the app does not has permission then the user will be prompted to grant permissions
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        // Check if we have write permission
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
     //Widgets
     public EditText message;
     public EditText key;
     public RadioGroup radioGroup;
+    public TextView display;
 
     //Global arrays
     public ArrayList config = new ArrayList();
@@ -33,10 +71,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verifyStoragePermissions(this);
 
         message = (EditText)findViewById(R.id.message);
         key = (EditText)findViewById(R.id.key);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        display = (TextView) findViewById(R.id.display);
+        display.setVisibility(View.GONE);
     }
 
     //ENCRYPT
@@ -223,8 +264,68 @@ public class MainActivity extends AppCompatActivity {
         key.setText(newString);
     }
 
-    //FLOATING ACTION BUTTON
-    public void fab(View view){
+    public void fab (View view){
+        String state;
+        state = Environment.getExternalStorageState();
 
+        if(Environment.MEDIA_MOUNTED.equals(state)){
+            File root = Environment.getExternalStorageDirectory();
+            File path = new File(root.getAbsolutePath()+"/vigenere");
+
+            if(!path.exists()){
+                path.mkdir();
+            }
+
+            File file = new File(path,"keys.txt");
+            String Message = key.getText().toString()+"\n";
+
+            try{
+                FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+                fileOutputStream.write(Message.getBytes());
+                fileOutputStream.close();
+                key.setText("");
+                Toast.makeText(getApplicationContext(),"Key Saved!",Toast.LENGTH_LONG).show();
+            }
+
+            catch(FileNotFoundException e){
+                e.printStackTrace();
+            }
+
+            catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }else{
+            Toast.makeText(getApplicationContext(),"Path not found!",Toast.LENGTH_LONG).show();
+        }
     }
+
+    /*public void readMessage (View view){
+        File root = Environment.getExternalStorageDirectory();
+        File path = new File(root.getAbsolutePath()+"/vigenere");
+        File file = new File(path,"keys.txt");
+
+        String Message;
+
+        try{
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+
+            while((Message=bufferedReader.readLine())!=null){
+                stringBuffer.append(Message + "\n");
+            }
+            display.setText(stringBuffer.toString());
+            display.setVisibility(View.VISIBLE);
+        }
+
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }*/
 }
